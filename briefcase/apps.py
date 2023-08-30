d20c3cd280702dc82827f68d2e9ccc75c4b263e7
Leandro Jacome <ljacomera@ufpso.edu.co>
2023-08-30 00:14:32 -0500

    Personalizacion del entorno admin

--- briefcase/apps.py
+++ briefcase/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BriefcaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'briefcase'
+    verbose_name = 'Portafolio'
--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'briefcase.apps.BriefcaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,6 +107,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
